b1197c5c9ca4e74bd4cc62d9644167ee0ca10fd9
Martin Galik <gale1508@gmail.com>
2024-12-04 20:30:12 +0100

    Kreiranje tablice za završni rad

---
+++ NogometZavrsniwp7.sql
@@ -0,0 +1,42 @@
+﻿use master;
+
+go
+drop database if exists nogomet_zavr;
+go
+create database nogomet_zavr;
+go
+use nogomet_zavr;
+go
+
+create table natjecanja(
+sifra int not null primary key identity(1,1),
+naziv varchar(80) not null,
+vrsta varchar(50) not null,
+sezona datetime not null,
+mjesto_odigravanja varchar(80),
+pobjednik int not null,
+najbolji_igrac int,
+);
+
+create table timovi(
+sifra int not null primary key identity(1,1),
+naziv varchar(80) not null,
+klub_repka bit not null,
+najtecanje int not null references natjecanja(sifra),
+trener varchar(80),
+stadion varchar(80)
+);
+
+create table igraci(
+sifra int not null primary key identity(1,1),
+ime varchar(50) not null,
+prezime varchar(80) not null,
+dob int,
+golovi int not null,
+asistencije int not null
+);
+
+create table sezone(
+tim int not null references timovi(sifra),
+igrac int not null references igraci(sifra)
+);
